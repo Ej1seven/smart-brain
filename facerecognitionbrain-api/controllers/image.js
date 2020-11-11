e25ee2c79a08bcf67e68490f677078416902a3e5
Erik Hunter <ej1seven@gmail.com>
2020-11-10 21:16:39 -0600

    adding project files

---
+++ facerecognitionbrain-api/controllers/image.js
@@ -0,0 +1,30 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'a4a28c6cf78342a7966e144cea789562'
+   });
+
+   const handleApiCall = (req, res) => {
+   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+   .then(data => {
+       res.json(data);
+   })
+   .catch(err => res.status(400).json('unable to work with API'))
+   }
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+ };
